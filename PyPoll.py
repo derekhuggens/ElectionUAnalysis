eff0299d2309ba692a09bfb7396337175e1f4e1c
derekhuggens <derekhuggens@gmail.com>
2021-11-04 17:49:57 -0400

    Adding the election audit code.

--- PyPoll.py
+++ PyPoll.py
@@ -8,16 +8,30 @@ import os
 
 # Assign a variable for the file to load the path.
 file_to_load = os.path.join("Resources", "election_results.csv")
+# Create a filename variable to a direct or indirect path to the file.
+file_to_save = os.path.join("analysis", "election_analysis.txt")
 
 ### Less efficient open and close of a file.
 # Open the election results and read the file.
 # election_data = open(file_to_load, 'r')
-
 # To do: Perform analysis.
-
 # Close the file.
 # election_data.close()
 ###
+
+# Initialize a total vote counter. "ACCUMULATOR."
+total_votes = 0
+
+# Create a candidate list.
+candidate_options = []
+
+# Declare an empty dictionary.
+candidate_votes = {}
+
+# Winning Candidate and Winning Count Tracker.
+winning_candidate = ""
+winning_count = 0
+winning_percentage = 0
 
 # Open the election results and read the file.
 with open(file_to_load) as election_data:
@@ -26,32 +40,62 @@ with open(file_to_load) as election_data:
     # Read the file object with the reader function.
     file_reader = csv.reader(election_data)
 
-    # Read and print the header row
+    # Read and print the header row.
     headers = next(file_reader)
-    print(headers)
 
     # Print each row in the CSV file.
     for row in file_reader:
-        print(row)
+        # Add to the total vote count.
+        total_votes += 1
 
+        # Print the candidate name from each row.
+        candidate_name = row[2]
+        # If the candidate does not match any existing candidate...
+        if candidate_name not in candidate_options:
+            # Add the name to candidate_options list.
+            candidate_options.append(candidate_name)
+            # Begin tracking that candidate's vote count.
+            candidate_votes[candidate_name] = 0
+        # Add a vote to that candidate's count.
+        candidate_votes[candidate_name] += 1
 
-    # Print the file object
-    # print(election_data)
+# Determine the percentage of votes for each candidate by looping through the counts.
+# Iterate through the candidate list.
+    for candidate_name in candidate_votes:
+        # Retrieve the vote count of a candidate.
+        votes = candidate_votes[candidate_name]
+        # Calculate the percentage votes.
+        vote_percentage = float(votes) / float(total_votes) * 100
 
+        # To do: print out each candidate's name, vote count, and percentage of votes to the terminal.
+        print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
+
+        # Determine winning vote count and winning candidate.
+        # Determine if the votes is greater than the winning count.
+        if (votes > winning_count) and (vote_percentage > winning_percentage):
+            # If true then set winning_count = votes and winning_percentage = vote_percentage.
+            winning_count = votes
+            winning_percentage = vote_percentage
+            # And, set the winning_candidate equal to the candidate's name.
+            winning_candidate = candidate_name
+    winning_candidate_summary = (
+        f"---------------------------\n"
+        f"Winner: {winning_candidate}\n"
+        f"Winning Vote Count: {winning_count:,}\n"
+        f"Winning Percentage: {winning_percentage:.1f}%\n"
+        f"---------------------------\n")
+    print(winning_candidate_summary)
 
 ### This method is less concise when not using the 'with' statement.
 # Create a filename variable to a direct or indirect path to the file.
 # file_to_save = os.path.join("analysis", "election_analysis.txt")
-# Using the open() function with the "w" mode we will write data to the .
+# Using the open() function with the "w" mode we will write data to the...
 # outfile = open(file_to_save, "w")
 # Write some data to the file.
 # outfile.write("Hello World")
 # Close the file
 # outfile.close()
 ###
-
-# Create a filename variable to a direct or indirect path to the file.
-file_to_save = os.path.join("analysis", "election_analysis.txt")
 
 # Using the with statement to open the file as a text file.
 with open(file_to_save, "w") as txt_file:
@@ -61,14 +105,5 @@ with open(file_to_save, "w") as txt_file:
 
 
 
-
-
-
-
-# 1. Total number of votes cast.
-# 2. A complete list of candidates who received votes.
-# 3. Total number of votes each candidate received.
-# 4. Percentage of votes each candidate won.
-# 5. The winner of the election based on popular vote.
 
 
